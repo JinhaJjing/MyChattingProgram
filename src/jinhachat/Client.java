39bef4eee23fbe662a47b0984a4588c2eb824045
JinhaJjing <jinha_suh@tmax.co.kr>
2021-03-03 16:33:05 +0900

    Change client start logic

--- src/jinhachat/Client.java
+++ src/jinhachat/Client.java
@@ -3,18 +3,14 @@ package jinhachat;
 public class Client {
 
     public void clientStart() {
-        try (SocketChannel socketChannel = SocketChannel.open(new InetSocketAddress("localhost", 15000))) {
-            //EventHandler는 사용자가 키보드 입력과 서버로부터 오는 이벤트를 감지하고 처리
-            Thread eventHandler = new Thread(new EventHandler(socketChannel, this));
-            eventHandler.start();
+        ClientInfo clientInfo = new ClientInfo();
 
-            Thread systemIn = new Thread(new SystemIn(socketChannel, this));
-            systemIn.start();
+        //EventHandler는 사용자가 키보드 입력과 서버로부터 오는 이벤트를 감지하고 처리
+        EventHandler eventHandler = new EventHandler(clientInfo);
+        eventHandler.start();
 
-        } catch (IOException e) {
-            System.out.println("서버와 연결이 종료되었습니다.");
-            e.printStackTrace();
-        }
+        SystemIn systemIn = new SystemIn(eventHandler);
+        systemIn.start();
     }
 
     public static void main(String[] args) {
